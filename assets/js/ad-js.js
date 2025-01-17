96413e6800bcd0ced06605f1611661d1374413ca
tajulkhan <tajulbasheer@gmail.com>
2025-01-17 08:23:28 +0530

    Operands

--- assets/js/ad-js.js
+++ assets/js/ad-js.js
@@ -11,3 +11,23 @@ async function fetchSequentialData() {
   }
   
   fetchSequentialData();  
+
+  var x = 220;
+var y = "Hello";
+var z = undefined;
+        
+// x | | y    // Returns 220 since the first value is truthy
+        
+// x | | z   // Returns 220 since the first value is truthy
+        
+// x && y    // Returns "Hello" since both the values are truthy
+        
+// y && z   // Returns undefined since the second value is falsy
+        
+if( x && y ){ 
+  console.log("Code runs" ); // This block runs because x && y returns "Hello" (Truthy)
+}   
+        
+if( x || z ){
+  console.log("Code runs");  // This block runs because x || y returns 220(Truthy)
+}
