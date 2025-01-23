dc328c7d7a63d6e00af16ac78b0dc56d5489eef5
tajulkhan <tajulbasheer@gmail.com>
2025-01-23 23:46:28 +0530

    Explain ArrayBuffer and TypedArray.

--- assets/js/ad-js.js
+++ assets/js/ad-js.js
@@ -211,3 +211,8 @@ console.log(localStorage.getItem('key')); // 'value'
 // sessionStorage
 sessionStorage.setItem('key', 'value');
 console.log(sessionStorage.getItem('key')); // 'value'
+// Explain ArrayBuffer and TypedArray.
+let buffer = new ArrayBuffer(16);
+let int32View = new Int32Array(buffer);
+int32View[0] = 42;
+console.log(int32View[0]); // 42
