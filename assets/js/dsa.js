ad8e75ba1f6d4f3bfef18320757e2e6801effecb
tajulkhan <tajulbasheer@gmail.com>
2025-01-27 19:56:29 +0530

    mixedArray concept

--- assets/js/dsa.js
+++ assets/js/dsa.js
@@ -15,3 +15,17 @@ if (
   console.log("The evnet available");
 }
 
+
+const mixedArray = [[], {}, {}];
+
+if (Array.isArray(mixedArray[0]) && mixedArray[0].length === 0) {
+  console.log("First element is an empty array"); 
+}
+
+if (typeof mixedArray[1] === "object" && !Array.isArray(mixedArray[1]) && Object.keys(mixedArray[1]).length === 0) {
+  console.log("Second element is an empty object");
+}
+
+if (typeof mixedArray[2] === "object" && !Array.isArray(mixedArray[2]) && Object.keys(mixedArray[2]).length === 0) {
+  console.log("Third element is an empty object");
+}
