d9bb6bbe9a2cbe8fa7462115c58cba7b9a232d38
tajulkhan <tajulbasheer@gmail.com>
2025-01-28 23:01:01 +0530

    Find Factorial Remove Duplicates from an Array

--- assets/js/dsa.js
+++ assets/js/dsa.js
@@ -140,3 +140,19 @@ function countVowels(str) {
 }
 
 console.log(countVowels("hello")); // Output: 2
+
+// Find Factorial
+
+function factorial(n) {
+  if (n === 0 || n === 1) return 1;
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(5)); // Output: 120
+
+// Remove Duplicates from an Array
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // Output: [1, 2, 3, 4, 5]
