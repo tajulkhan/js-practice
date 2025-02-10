463a7e8d4cc49ff0dfe820aeff9ee5ab2bb9c6c7
tajulkhan <tajulbasheer@gmail.com>
2025-02-10 21:49:56 +0530

    Find the Majority Element

--- assets/js/dsa.js
+++ assets/js/dsa.js
@@ -730,3 +730,20 @@ const findMissingNumber = (arr, n) => {
 
 const arrFN = [1, 2, 4, 5, 6];
 console.log("Missing number:", findMissingNumber(arrFN, 6));
+
+// Find the Majority Element
+const majorityElement = (nums) => {
+  let candidate = null;
+  let count = 0;
+
+  for (let num of nums) {
+    if (count === 0) {
+      candidate = num;
+    }
+    count += (num === candidate) ? 1 : -1;
+  }
+
+  return candidate;
+};
+console.log(majorityElement([3, 3, 4, 2, 3, 3, 3])); // Output: 3
+console.log(majorityElement([2, 2, 1, 1, 1, 2, 2])); // Output: 2
