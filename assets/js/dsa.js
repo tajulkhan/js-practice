ff3ec0cb301abb3f3ba896af634d329e8429a159
tajulkhan <tajulbasheer@gmail.com>
2025-02-11 20:56:40 +0530

    function createCounter

--- assets/js/dsa.js
+++ assets/js/dsa.js
@@ -777,3 +777,27 @@ function expect(val) {
 		}
 	}
 }
+
+// Write a function createCounter. It should accept an initial integer init. It should return an object with three functions.
+// The three functions are:
+// increment() increases the current value by 1 and then returns it.
+// decrement() reduces the current value by 1 and then returns it.
+// reset() sets the current value to init and then returns it.
+function createCounter(init) {
+  let current = init;
+
+  return {
+      increment: function() {
+          current++;
+          return current;
+      },
+      decrement: function() {
+          current--;
+          return current;
+      },
+      reset: function() {
+          current = init;
+          return current;
+      }
+  };
+}
