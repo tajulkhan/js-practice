599087dcf1d208d6359b571b1c1982780eb09f0a
tajulkhan <tajulbasheer@gmail.com>
2025-02-25 23:27:55 +0530

    ++

--- assets/js/ad-js.js
+++ assets/js/ad-js.js
@@ -630,16 +630,14 @@ console.log(person2)
 function divideByHalf(sum){
   console.log(Math.floor(sum / 2));
 }
-
 function multiplyBy2(sum){
   console.log(sum * 2);
 }
-
 function operationOnSum(num1,num2,operation){
   var sum = num1 + num2;
   operation(sum);
 }
+operationOnSum(3, 3, divideByHalf); 
+operationOnSum(5, 5, multiplyBy2); 
 
-operationOnSum(3, 3, divideByHalf); // Outputs 3
 
-operationOnSum(5, 5, multiplyBy2); // Outputs 20
