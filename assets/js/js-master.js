8a6e5345a6340f75bd697dbd89f4c83eab67a597
Tajul Khan BS <78636360+tajulkhan@users.noreply.github.com>
2025-04-04 17:42:22 +0530

    Update 🔍  filter()

--- assets/js/js-master.js
+++ assets/js/js-master.js
@@ -73,3 +73,9 @@ getData();
 const numbers = [1, 2, 3, 4];
 const doubled = numbers.map((num) => num * 2);
 console.log(doubled); // [2, 4, 6, 8]
+
+// 🔍 2. filter()
+const numbers = [1, 2, 3, 4, 5];
+
+const evens = numbers.filter((num) => num % 2 === 0);
+console.log(evens); // [2, 4]
