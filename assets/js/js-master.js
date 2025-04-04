48fc647951ae75a03058fd14753eb08dab0c03ff
Tajul Khan BS <78636360+tajulkhan@users.noreply.github.com>
2025-04-04 16:45:48 +0530

    Update js-master.js

--- assets/js/js-master.js
+++ assets/js/js-master.js
@@ -1,1 +1,21 @@
 // Core JavaScript Concepts to Master
+
+// closure function
+function createCounter() {
+  let count = 0;
+  return {
+    increment: () => ++count,
+    decrement: () => --count,
+    getCount: () => count
+  };
+}
+
+// Create a new counter instance
+const counter = createCounter();
+
+// Use the methods and log their output
+console.log("Initial count:", counter.getCount());  // 0
+console.log("Incrementing count:", counter.increment());  // 1
+console.log("Incrementing count again:", counter.increment());  // 2
+console.log("Decrementing count:", counter.decrement());  // 1
+console.log("Final count:", counter.getCount());  // 1
