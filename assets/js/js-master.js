222583e5108745a9391afb93e4052751d73bab90
Tajul Khan BS <78636360+tajulkhan@users.noreply.github.com>
2025-04-07 17:23:30 +0530

    Update  Rewrite 'if..else' into '?'

--- assets/js/js-master.js
+++ assets/js/js-master.js
@@ -291,3 +291,10 @@ if (login == 'Employee') {
 }
 console.log(message);
 
+let message = (login == 'Employee') ? 'Hello' :
+  (login == 'Director') ? 'Greetings' :
+  (login == '') ? 'No login' :
+  '';
+console.log(message);
+
+
