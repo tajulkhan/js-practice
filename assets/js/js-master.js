c3411c311acebfea02821b8d9a14557327cf7c40
Tajul Khan BS <78636360+tajulkhan@users.noreply.github.com>
2025-04-09 10:25:55 +0530

    Update Object methods, "this"

--- assets/js/js-master.js
+++ assets/js/js-master.js
@@ -459,3 +459,18 @@ user.sizes.width = 60;    // change a property from one place
 alert(clone.sizes.width); // 50, not related
 console.log(user)
 console.log(clone)
+
+// Object methods, "this"
+let user = { name: "John" };
+let admin = { name: "Admin" };
+function sayHi() {
+  alert( this.name );
+}
+// use the same function in two objects
+user.f = sayHi;
+admin.f = sayHi;
+// these calls have different this
+// "this" inside the function is the object "before the dot"
+user.f(); // John  (this == user)
+admin.f(); // Admin  (this == admin)
+admin['f'](); // Admin (dot or square brackets access the method – doesn't matter)
