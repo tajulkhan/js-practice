771d94f166c4e0ba3e81d820d7931e069b628e4b
Tajul Khan BS <78636360+tajulkhan@users.noreply.github.com>
2025-04-09 10:14:44 +0530

    Update structured clone

--- assets/js/js-master.js
+++ assets/js/js-master.js
@@ -443,3 +443,19 @@ let clone = Object.assign({}, user);
 alert( user.sizes === clone.sizes ); 
 user.sizes.width = 60;    
 alert(clone.sizes.width); 
+
+// structuredClone
+let user = {
+  name: "John",
+  sizes: {
+    height: 182,
+    width: 50
+  }
+};
+let clone = structuredClone(user);
+alert( user.sizes === clone.sizes ); // false, different objects
+// user and clone are totally unrelated now
+user.sizes.width = 60;    // change a property from one place
+alert(clone.sizes.width); // 50, not related
+console.log(user)
+console.log(clone)
