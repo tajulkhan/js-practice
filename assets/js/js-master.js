146e18f1c22dbafbd08508205f3633f0d140c289
Tajul Khan BS <78636360+tajulkhan@users.noreply.github.com>
2025-04-10 10:41:58 +0530

    Update Constructor mode test: new.target

--- assets/js/js-master.js
+++ assets/js/js-master.js
@@ -508,3 +508,13 @@ function User(name) {
 let user = new User("Jack");
 alert(user.name); // Jack
 alert(user.isAdmin); // false
+
+// Constructor mode test: new.target
+function User(name) {
+  if (!new.target) { // if you run me without new
+    return new User(name); // ...I will add new for you
+  }
+  this.name = name;
+}
+let john = User("John"); // redirects call to new User
+alert(john.name); // John
