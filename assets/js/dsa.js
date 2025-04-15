df059104bc98ed07265db7f548a7cb0215192282
Tajul Khan BS <78636360+tajulkhan@users.noreply.github.com>
2025-04-15 09:45:12 +0530

    Update Find the Majority Element

--- assets/js/dsa.js
+++ assets/js/dsa.js
@@ -986,3 +986,17 @@ function groupAnagrams(strs) {
 }
 
 console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
+
+// Find the Majority Element
+function majorityElement(nums) {
+  let count = 0, candidate = null;
+  for (let num of nums) {
+    if (count === 0) {
+      candidate = num;
+    }
+    count += (num === candidate) ? 1 : -1;
+  }
+  return candidate;
+}
+console.log(majorityElement([2, 2, 1, 1, 1, 2, 2])); // 2
+
