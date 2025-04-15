99ffe77a88100df8e06f8c7363e6246173ec89fc
Tajul Khan BS <78636360+tajulkhan@users.noreply.github.com>
2025-04-15 09:46:55 +0530

    Update Longest Common Prefix (String Array)

--- assets/js/dsa.js
+++ assets/js/dsa.js
@@ -1000,3 +1000,20 @@ function majorityElement(nums) {
 }
 console.log(majorityElement([2, 2, 1, 1, 1, 2, 2])); // 2
 
+// Longest Common Prefix (String Array)
+function longestCommonPrefix(strs) {
+  if (!strs.length) return "";
+
+  let prefix = strs[0];
+
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.slice(0, -1);
+      if (prefix === "") return "";
+    }
+  }
+
+  return prefix;
+}
+
+console.log(longestCommonPrefix(["flower", "flow", "flight"])); // "fl"
