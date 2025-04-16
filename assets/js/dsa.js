48b62d329ab5da49565931bb160d3e07259df9f3
Tajul Khan BS <78636360+tajulkhan@users.noreply.github.com>
2025-04-16 10:08:49 +0530

    Update Maximum Subarray Sum (Kadane’s Algorithm) – O(n)

--- assets/js/dsa.js
+++ assets/js/dsa.js
@@ -1061,3 +1061,14 @@ function isIsomorphic(s, t) {
 console.log(isIsomorphic("egg", "add")); // true
 console.log(isIsomorphic("foo", "bar")); // false
 
+ // Maximum Subarray Sum (Kadane’s Algorithm) – O(n)
+function maxSubArray(nums) {
+  let maxSum = nums[0];
+  let currentSum = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    currentSum = Math.max(nums[i], currentSum + nums[i]);
+    maxSum = Math.max(maxSum, currentSum);
+  }
+  return maxSum;
+}
+console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4])); // 6
