d24fa642e3327d86d07d7781d7a1d271515f660a
Tajul Khan BS <78636360+tajulkhan@users.noreply.github.com>
2025-04-17 11:21:06 +0530

    Update First Unique Character in a String

--- assets/js/dsa.js
+++ assets/js/dsa.js
@@ -1122,3 +1122,19 @@ function isAnagram(str1, str2) {
 
 console.log(isAnagram("listen", "silent")); // true
 
+// First Unique Character in a String
+function firstUniqChar(s) {
+  let freq = {};
+
+  for (let char of s) {
+    freq[char] = (freq[char] || 0) + 1;
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    if (freq[s[i]] === 1) return i;
+  }
+
+  return -1;
+}
+
+console.log(firstUniqChar("leetcode")); // 0
