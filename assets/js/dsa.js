8ac4eed489358a9fa5d69a5c52c42b5290127b54
Tajul Khan BS <78636360+tajulkhan@users.noreply.github.com>
2025-04-17 11:23:01 +0530

    Update Compress a String (Run-Length Encoding)

--- assets/js/dsa.js
+++ assets/js/dsa.js
@@ -1160,3 +1160,22 @@ function groupByCharFrequency(words) {
 
 console.log(groupByCharFrequency(["abc", "bca", "cab", "aabb", "baba", "aaabb"]));
 
+// Compress a String (Run-Length Encoding)
+function compressString(str) {
+  let result = "";
+  let count = 1;
+
+  for (let i = 1; i <= str.length; i++) {
+    if (str[i] === str[i - 1]) {
+      count++;
+    } else {
+      result += str[i - 1] + count;
+      count = 1;
+    }
+  }
+
+  return result;
+}
+
+console.log(compressString("aaabbcddd")); // "a3b2c1d3"
+
