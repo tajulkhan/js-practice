275aa63539da7e25690a2904d6ccfc2c41119b89
Tajul Khan BS <78636360+tajulkhan@users.noreply.github.com>
2025-04-19 10:11:56 +0530

    Update js-dsa.js
    Parse URL Query Params to Object

--- js-dsa.js
+++ js-dsa.js
@@ -1,1 +1,16 @@
+//Parse URL Query Params to Object
+function parseQuery(queryString) {
+  const result = {};
+  queryString = queryString.startsWith("?") ? queryString.slice(1) : queryString;
 
+  const pairs = queryString.split("&");
+
+  for (let pair of pairs) {
+    let [key, value] = pair.split("=");
+    result[decodeURIComponent(key)] = decodeURIComponent(value || "");
+  }
+
+  return result;
+}
+
+console.log(parseQuery("?name=Taj&role=dev&city=Chennai"));
