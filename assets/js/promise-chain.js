aa4e9ffe6c40f29e44d682feba936d8aee78b66f
Tajul Khan BS <78636360+tajulkhan@users.noreply.github.com>
2025-04-23 11:17:53 +0530

    Update Same with Async/Await

--- assets/js/promise-chain.js
+++ assets/js/promise-chain.js
@@ -10,3 +10,12 @@ function stp2(prev){
 stp1()
     .then(rslt => stp2(rslt))
     .then(final => console.log(final));
+
+// Same with Async/Await ^^^^
+async function runTsk(){
+    const s1 = await stp1();
+    const s2 = await stp2(s1);
+    console.log(s2);
+}
+
+runTsk();
