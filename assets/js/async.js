fef8602467f063100d36bc90b727685c51c55f6b
Tajul Khan BS <78636360+tajulkhan@users.noreply.github.com>
2025-04-29 11:19:53 +0530

    Update forEach doesn't await async callbacks

--- assets/js/async.js
+++ assets/js/async.js
@@ -1,1 +1,12 @@
 // Mastering async loops + clean error handling in for, forEach, and map.
+const urls = ['url1', 'url2', 'url3'];
+
+urls.forEach(async (url) => {
+  const res = await fetch(url);   
+  const data = await res.json();
+  console.log(data);
+});
+// ❌ this won't wait!
+
+
+
