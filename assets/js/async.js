c8e32f53c071568a3341492ab40b4714b4fb847c
Tajul Khan BS <78636360+tajulkhan@users.noreply.github.com>
2025-05-01 13:10:53 +0530

    Update async.js

--- assets/js/async.js
+++ assets/js/async.js
@@ -1,6 +1,5 @@
 // Mastering async loops + clean error handling in for, forEach, and map.
 const urls = ['url1', 'url2', 'url3'];
-
 urls.forEach(async (url) => {
   const res = await fetch(url);   
   const data = await res.json();
