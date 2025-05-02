143a7489f1bbd0180375282d2c008845c380d644
Tajul Khan BS <78636360+tajulkhan@users.noreply.github.com>
2025-05-02 10:45:56 +0530

    Update Closures (Base-Level  Concept)

--- assets/js/int-prep.js
+++ assets/js/int-prep.js
@@ -1,1 +1,13 @@
+// A closure is when a function “remembers” variables from its lexical scope even when that function is executed outside of that scope.
 
+// Closures (Base-Level  Concept)
+function outer(){
+    let counter = 0;
+    return function inner(){
+        counter ++;
+        console.log(counter);
+    }
+}
+const fn = outer();
+fn();
+fn();
