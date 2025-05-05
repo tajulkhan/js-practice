cdd0ed4bb5f68046e1e1d53a4721477f99b79456
Tajul Khan BS <78636360+tajulkhan@users.noreply.github.com>
2025-05-05 10:39:37 +0530

    Update JavaScript Recursion Program to countdown till 1

--- assets/js/async.js
+++ assets/js/async.js
@@ -194,3 +194,23 @@ console.log(double);
 setTimeout(()=>{
     console.log("Hii")
 }, 1500)
+
+// JavaScript Recursion Program to countdown till 1
+// recursive function
+function counter(count) {
+    // display count
+    console.log(count);
+    // condition for stopping
+    if(count > 1) {
+        // decrease count
+        count = count - 1;
+        // call counter with new value of count
+        counter(count);
+    } else {
+        // terminate execution
+        return;
+    };
+};
+// access function
+counter(5);
+
