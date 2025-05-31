a6611a1e7dc20cccfeac746d90bc60574d8729fb
Tajul Khan BS <78636360+tajulkhan@users.noreply.github.com>
2025-05-31 10:31:30 +0530

    Update Debouncing

--- js-dsa.js
+++ js-dsa.js
@@ -286,4 +286,31 @@ const users = [
 ];
 console.log(groupBy(users, "role"));
 
+// Debouncing in JavaScript
+// Debounce function
+function debounce(func, delay) {
+    let timeout;
+    return function (...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+            func.apply(this, args);
+        }, delay);
+    };
+}
+
+// Function to be debounced
+function search(query) {
+    console.log('Searching for:', query);
+}
+
+// Create a debounced version of the search function
+const dSearch = debounce(search, 100);
+
+// Simulate typing with multiple calls to the debounced function
+dSearch('Hello');
+dSearch('Hello, ');
+dSearch('Hello, World!'); 
+
+
+
 
