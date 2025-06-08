64c8d24b4bbe55b639024ba92b899551831aca78
Tajul Khan BS <78636360+tajulkhan@users.noreply.github.com>
2025-06-08 08:05:39 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,14 +1,12 @@
 // Boosting API Performance with Parallel Requests!
 // Instead of calling APIs one by one (which is slow and inefficient), I implemented a smarter solution using Promise.all() to fetch multiple product details simultaneously from the Fake Store API.
 // ✅ Faster execution ✅ Cleaner code ✅ Better user experience
-
 const apiEndPoints = [
 "https://fakestoreapi.com/products/1",
 "https://fakestoreapi.com/products/2",
 "https://fakestoreapi.com/products/3",
 "https://fakestoreapi.com/products/4",
 "https://fakestoreapi.com/products/5"
-
 ];
 const fetchProductData = async (urls) => {
     try{
