02429ca82a7677d2af724acaae260fc0a00a7649
Tajul Khan BS <78636360+tajulkhan@users.noreply.github.com>
2025-06-23 10:55:30 +0530

    Update access the elements of an array

--- array.js
+++ array.js
@@ -4,3 +4,6 @@
 // Arrays can hold values of any data type, and each value is accessed using an index, with the first index being 0.
 // Declaring an array with three elements
 let fruits = ['apple', 'banana', 'cherry'];
+
+// 2. How do you access the elements of an array?
+// Elements in an array are accessed using their index. The index of the first element is 0, the second element is 1, and so on.
