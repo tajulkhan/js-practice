6178cf01c6812db1fbae886732891f2df1687b7a
Tajul Khan BS <78636360+tajulkhan@users.noreply.github.com>
2025-06-23 10:53:31 +0530

    Update array

--- array.js
+++ array.js
@@ -1,1 +1,4 @@
 //Array concept basic and intermediated and advanced 
+// 1. What is an array in JavaScript? How do you declare an array?
+// An array in JavaScript is a data structure that allows you to store multiple values in a single variable. 
+// Arrays can hold values of any data type, and each value is accessed using an index, with the first index being 0.
