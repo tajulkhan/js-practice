68ac1603351bad6a51097530deb35b37dae214e1
tajulkhan <tajulbasheer@gmail.com>
2025-07-18 00:04:17 +0530

    Async Arrow Functions

--- array.js
+++ array.js
@@ -212,3 +212,7 @@ const makePerson = (firstName, lastName) =>
 console.log(makePerson("Pankaj", "Bind"));
 // Async Arrow Functions
 // Arrow functions can be made asynchronous by adding the async keyword before the parameter list.
+const fetchData = async () => {
+    const data = await fetch('https://api.example.com/data');
+    return data.json();
+};
